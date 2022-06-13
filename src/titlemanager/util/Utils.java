7796c72522b6996e6b328e7246bfe3a047562e73
Daffa Faris <daffafaris87@gmail.com>
2022-06-13 11:44:15 +0700

    relocate package

---
+++ src/titlemanager/util/Utils.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package titlemanager.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Fure
+ */
+public class Utils {
+    public static String encryptPassword(String password){
+        String encryptedpassword = null;  
+        try {  
+            MessageDigest m = MessageDigest.getInstance("MD5");
+            m.update(password.getBytes());
+            byte[] bytes = m.digest();
+            StringBuilder s = new StringBuilder();  
+            for(int i=0; i< bytes.length ;i++)  
+            {  
+                s.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));  
+            }
+            encryptedpassword = s.toString();  
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return encryptedpassword;
+    }
+}
